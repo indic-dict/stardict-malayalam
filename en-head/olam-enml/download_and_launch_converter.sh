21b088668d248bdd546c098489c221ba3ffd7888
damooo <damodara@protonmail.com>
2018-06-28 19:58:33 +0530

    olam-enml added.

---
+++ en-head/olam-enml/download_and_launch_converter.sh
@@ -0,0 +1,3 @@
+wget -O olam-enml.csv.zip https://olam.in/open/enml/olam-enml.csv.zip
+unzip olam-enml.csv.zip olam-enml.csv
+wget -O parts-of-speech.txt https://olam.in/open/enml/parts-of-speech.txt
